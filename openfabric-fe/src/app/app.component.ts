67438d7db414987919ba861c3cc08871d99f66db
Pop Emanuel <56311915+PopEmanuel@users.noreply.github.com>
2023-08-15 11:34:13 +0300

    Add frontend design

--- openfabric-fe/src/app/app.component.ts
+++ openfabric-fe/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'openfabric-fe';
+  containerStatus: string = 'running'; // Set the initial status, or get it from your data
+
+  getStatusClass(): string {
+    return this.containerStatus === 'running' ? 'green' : 'red';
+  }
 }
